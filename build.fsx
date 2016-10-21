96826fb9e63ab944d2017b1b4be63786f702467a
krauthaufen <krauthaufen@awx.at>
2016-10-21 15:39:51 +0200

    installed new package

--- build.fsx
+++ build.fsx
@@ -9,7 +9,7 @@ open Aardvark.Fake
 
 do Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-DefaultSetup.install ["src/DependentFiles.sln"]
+DefaultSetup.install ["src/Aardvark.Compiler.DomainTypes.sln"]
 
 
 entry()
