e7cebd7213f69fdf4e2253b9a8486d72bf04d8f1
Harald Steinlechner <haraldsteinlechner@gmail.com>
2017-06-14 17:23:59 +0200

    sync

--- src/Aardvark.Compiler.DomainTypeTool/Program.fs
+++ src/Aardvark.Compiler.DomainTypeTool/Program.fs
@@ -50,8 +50,65 @@ let crackProject (file : string) =
             references = references
         }
     with e ->
-        printfn "[cracking project] %A at %A" e.Message e.StackTrace
+        printfn "[cracking project] with message: %s at:\n\n%A" e.Message e.StackTrace
         None
+
+let doIt proj =
+    let sw = System.Diagnostics.Stopwatch()
+    printf "cracking project: "
+    sw.Start()
+    let cracked = crackProject proj
+    sw.Stop()
+    match cracked with
+        | Some project ->
+            printfn "done (%.0fs)" sw.Elapsed.TotalSeconds
+
+            printf "processing: "
+            sw.Restart()
+            let res = Preprocessing.run project.path project.references project.files |> Async.RunSynchronously
+            match res with
+                | Worked res ->
+                    sw.Stop()
+                    printfn "done (%.0fs)" sw.Elapsed.TotalSeconds
+                    printfn "writing output"
+                    for (f,prep) in Map.toSeq res do
+                        match prep with
+                            | Finished(warnings, content) ->
+                                let path = System.IO.Path.ChangeExtension(f, ".g.fs")
+                                printf "    %s: " (Path.GetFileName f)
+
+                                for w in warnings do 
+                                    printfn ""
+                                    printf "        WARNING: %A" w
+
+                                let old = 
+                                    if File.Exists path then File.ReadAllText path
+                                    else ""
+
+                                if old <> content then
+                                    File.WriteAllText(path, content)
+
+                                if List.isEmpty warnings then
+                                    printfn "done"
+                                else
+                                    printfn ""
+
+                            | Faulted(warnings, err) ->
+                                printfn "    %s: " (Path.GetFileName f)
+                                for w in warnings do 
+                                    printfn "        WARNING: %A" w
+
+                                printfn "        ERROR: %A" err
+                    0
+                | CompilerError errors ->
+                    sw.Stop()
+                    printfn "faulted"
+                    for e in errors do
+                        printfn "    ERROR: %A" e
+                    -1
+        | None ->
+            printfn "faulted"
+            -1
 
 
 [<EntryPoint>]
@@ -61,60 +118,10 @@ let main argv =
         printfn "usage: %s <fsprojpath>" me
         -1
     else
-        let sw = System.Diagnostics.Stopwatch()
         let proj = argv.[0]
-        
-        printf "cracking project: "
-        sw.Start()
-        let cracked = crackProject proj
-        sw.Stop()
-        match cracked with
-            | Some project ->
-                printfn "done (%.0fs)" sw.Elapsed.TotalSeconds
 
-                printf "processing: "
-                sw.Restart()
-                let res = Preprocessing.run project.path project.references project.files |> Async.RunSynchronously
-                match res with
-                    | Worked res ->
-                        sw.Stop()
-                        printfn "done (%.0fs)" sw.Elapsed.TotalSeconds
-                        printfn "writing output"
-                        for (f,prep) in Map.toSeq res do
-                            match prep with
-                                | Finished(warnings, content) ->
-                                    let path = System.IO.Path.ChangeExtension(f, ".g.fs")
-                                    printf "    %s: " (Path.GetFileName f)
-
-                                    for w in warnings do 
-                                        printfn ""
-                                        printf "        WARNING: %A" w
-
-                                    let old = 
-                                        if File.Exists path then File.ReadAllText path
-                                        else ""
-
-                                    if old <> content then
-                                        File.WriteAllText(path, content)
-
-                                    if List.isEmpty warnings then
-                                        printfn "done"
-                                    else
-                                        printfn ""
-
-                                | Faulted(warnings, err) ->
-                                    printfn "    %s: " (Path.GetFileName f)
-                                    for w in warnings do 
-                                        printfn "        WARNING: %A" w
-
-                                    printfn "        ERROR: %A" err
-                        0
-                    | CompilerError errors ->
-                        sw.Stop()
-                        printfn "faulted"
-                        for e in errors do
-                            printfn "    ERROR: %A" e
-                        -1
-            | None ->
-                printfn "faulted"
-                -1
+        if File.Exists proj then 
+            doIt proj
+        else 
+            printfn "could not read project file. Make sure that the path exists and points to a .fsproj file."
+            -1
