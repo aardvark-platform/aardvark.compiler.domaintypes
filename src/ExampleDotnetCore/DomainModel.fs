165b03a36214fe997b2650b417b98e9398637fa3
Harald Steinlechner <haraldsteinlechner@gmail.com>
2018-04-13 17:51:37 +0200

    works for dotnetcore

---
+++ src/ExampleDotnetCore/DomainModel.fs
@@ -0,0 +1,83 @@
+﻿namespace DomainModel
+
+open Aardvark.Base
+open Aardvark.Base.Incremental
+
+type Model =
+    {
+        fileName    : string
+        bounds      : Box3d
+    }
+
+[<DomainType>]
+type MyUnion =
+    | A of int * hset<int>
+    | B of string
+    | C
+
+[<DomainType>]
+type Object =
+    {
+        [<PrimaryKey>]
+        name        : string
+        sepp        : string
+        trafo       : Trafo3d
+        model       : Model
+        blubber     : plist<MyUnion>
+    }
+
+[<DomainType>]
+type Symbol = V2i of int 
+
+[<DomainType>]
+type U = {
+    [<PrimaryKey>]
+    id : int 
+  }
+
+[<DomainType>]
+type U2 = { 
+    [<PrimaryKey>]
+    id : int 
+ } // duplcate
+
+[<DomainType>]
+type State =
+    {
+        [<TreatAsValue>]
+        past        : Option<State>
+
+        [<NonIncremental>]
+        hugo : int
+
+        primary     : Option<Object>
+        viewTrafo   : Trafo3d
+        objects     : hset<Object>
+        [<TreatAsValue>]
+        test        : plist<Object>
+
+        [<NonIncremental>]
+        threads     : hmap<int, Object>
+
+        testBlub : option<int>
+
+        shouldBeU : hset<U>
+    }
+
+[<DomainType>]
+type Tree =
+    {
+        name : string
+        id : plist<Tree>
+    }
+
+namespace Groups
+
+open Aardvark.Base
+open Aardvark.Base.Incremental
+
+[<DomainType>]
+type Researcher = { urdar : int }
+
+[<DomainType>]
+type Manager = { gabbl : int }
---
+++ src/ExampleDotnetCore/DomainModel2.fs
@@ -0,0 +1,53 @@
+﻿namespace DomainModel2
+
+open Aardvark.Base
+open Aardvark.Base.Incremental
+open DomainModel
+
+[<DomainType>]
+type Node<'a> = { value : 'a; children : plist<Node<'a>> }
+
+[<DomainType>]
+type Tree<'a> = { roots : plist<Node<'a>> }
+
+[<DomainType>]
+type Thing = 
+    { 
+        [<PrimaryKey>]
+        id : string
+        name: string 
+    }
+
+[<DomainType>]
+type MyState = 
+    { 
+        tree : Tree<Thing>
+        intTree : Tree<int> 
+        blubber : hset<U2>    
+    }
+
+
+[<DomainType>]
+type Tab = { name : string; url : string }
+
+[<DomainType>]
+type Tree2 = 
+    | Vertical of Tree2 * Tree2
+    | Horizontal of Tree2 * Tree2
+    | Leaf of Tab
+
+[<DomainType>]
+type Model = { 
+    tabs : list<Tab>
+}
+
+type Action = Action
+
+
+open Groups
+
+[<DomainType>]
+type Persons =
+    | Researchers of plist<Researcher>
+    | Managers of plist<Manager>
+    | BothOida of plist<Researcher> * plist<Manager> * plist<Manager>
