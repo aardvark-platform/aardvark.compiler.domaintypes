0d89891b0bb4fa1df9825bffb1807564a24b3efb
Harald Steinlechner <haraldsteinlechner@gmail.com>
2018-11-15 18:20:28 +0100

    better demo

---
+++ src/Demo/Demo.fs
@@ -0,0 +1,27 @@
+﻿namespace Demo
+
+open Aardvark.Base
+open Aardvark.Base.Incremental
+
+[<DomainType>]
+type Thing = { [<PrimaryKey>] value : int }
+
+[<DomainType>]
+type Example = 
+    {
+        value : int
+        hset : hset<int>
+        hsetM : hset<Thing>
+        plist : plist<Thing>
+        map : hmap<int,Thing>
+        list : list<Thing>
+        plainArray : array<int>
+    }
+
+[<DomainType>]
+type UnionExample =
+    | Thrice of int * int * int
+    | Once
+
+[<DomainType>]
+type GenericExample<'a> = { single : 'a; many : hset<'a>}
---
+++ src/Demo/Program.fs
@@ -0,0 +1,42 @@
+﻿open System
+
+open Demo
+open Aardvark.Base
+open Aardvark.Base.Incremental
+
+
+[<EntryPoint>]
+let main argv =
+
+    let thing = { value = 1 }
+    let mthing = MThing.Create(thing)
+    let adaptiveCode = 
+        mthing.value |> Mod.map (sprintf "%A") 
+
+    printfn "%A" (Mod.force adaptiveCode)
+
+    let newThing = { value = 10 }
+    transact (fun _ -> mthing.Update(newThing))
+    printfn "%A" (Mod.force adaptiveCode)
+
+    let example =
+        {
+            value = 0
+            hset  = HSet.empty
+            hsetM = HSet.empty
+            plist = PList.empty
+            map   = HMap.empty
+            list   = []
+            plainArray = [||]
+        }
+
+    let mExample = MExample.Create example
+    let value : IMod<int>    = mExample.value
+    let hset  : aset<int>    = mExample.hset
+    let hsetM : aset<MThing> = mExample.hsetM
+    let plist : alist<MThing> = mExample.plist
+    let map   : amap<int,MThing>  = mExample.map
+    let list  : IMod<list<Thing>> = mExample.list
+    let plainArray : IMod<array<int>> = mExample.plainArray
+
+    0 
